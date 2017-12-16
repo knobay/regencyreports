3af01189f203330701ff7981329021e3e1a7eb0e
Kim Nobay <kim@nobay.co.uk>
2017-12-16 18:05:24 +0000

    Collected most of the useful fields from jira

--- library/datacollector.py
+++ library/datacollector.py
@@ -1,6 +1,8 @@
 """Basic functions for datacollection, manipulation and saving functions for Regency Reports"""
 
+import getpass
 import requests # See http://docs.python-requests.org/en/master/user/install/#install"
+import json
 
 def read(target_url):
     "Gets json data from a REST API and returns a list of json objects"
@@ -8,16 +10,48 @@ def read(target_url):
     json_list = my_request.json()
     return json_list
 
-def read_authenticated(target_url):
-    "Gets json data from a REST API and returns a list of json objects"
-    login = input('enter username for jira project')
-    passw = input('enter password for jira project')
-    my_request = requests.get(target_url, auth=(login, passw))
+def read_jira_with_login(target_url):
+    "Gets Jira issues, user enters password"
+    print('connecting to Jira on \n{0}\nNeed account details...'.format(target_url))
+    login = input('Username: ')
+    pswd = getpass.getpass('Password:')
+    my_request = requests.get(target_url, auth=(login, pswd))
     json_list = my_request.json()
     return json_list
 
+def flatten_issues(issueslist):
+    "Extract properties of an issue and put in 2d issues array"
+    json_issues = issueslist['issues']
+    flatlist = []
+    for issue in json_issues:
+        issue_fields = {}
+        issue_fields['summary'] = issue['fields']['summary']
+        issue_fields['created'] = issue['fields']['created']
+        issue_fields['updated'] = issue['fields']['updated']
+        
+        if issue['fields']['assignee'] != None:
+            issue_fields['assignee'] = issue['fields']['assignee']['emailAddress']
+        else:
+            issue_fields['assignee'] = 'Not assigned'
+
+        
+        if issue['fields']['description']:
+            issue_fields['description'] = issue['fields']['description'].replace('\r', ' ').replace('\n', ' ').replace('\t', ' ')
+        else:
+            issue_fields['description'] = 'No description'
+        issue_fields['creator'] = issue['fields']['creator']['emailAddress']
+        issue_fields['reporter'] = issue['fields']['reporter']['emailAddress']
+        issue_fields['priority'] = issue['fields']['priority']['name']
+        issue_fields['type'] = issue['fields']['issuetype']['name']
+        issue_fields['project'] = issue['fields']['project']['name']
+        issue_fields['status'] = issue['fields']['status']['name']
+        issue_fields['url'] = issue['self']
+        issue_fields['id'] = issue['key']
+        flatlist.append(issue_fields)
+    return flatlist
+
 def tabulate(json_records, json_fields):
-    "Converts list of json records into a table containing the fields requested"
+    "Converts list of json records into a 2d array containing the fields requested"
     table = []
     row = []
     # Add headings in the first row of the table using the field names requested
@@ -34,6 +68,8 @@ def tabulate(json_records, json_fields):
 
 def save(list_to_write, file_name):
     "Writes a 2D list to a tab delimited file"
+    print('Saving data to {0}...'.format(file_name))
     with open(file_name, 'w') as thefile:
         thefile.writelines('\t'.join(i) + '\n' for i in list_to_write)
+    print('Done!')
     return True
